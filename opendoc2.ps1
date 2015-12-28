d79e320b331c78cec5d809e5cde75eb430e4860a
tguglanaklona <tguglanaklona@gmail.com>
2015-12-28 23:43:45 +0300

    m

---
+++ opendoc2.ps1
@@ -0,0 +1,26 @@
+#
+# Open new document with the text and delete ref
+#
+
+$docFilename = "docfile.rtf"
+$refFileName = "docfile.rtf.lnk"
+$docText = "some text"
+
+$path = (Get-Item -Path ".\" -Verbose).FullName
+$filename = $path + "\" + $docFilename
+
+$msWord = New-Object -Com Word.Application
+$msWord.visible = $false
+
+$wordDoc = $msWord.Documents.add()
+$wordDoc.Activate()
+
+$selection = $msWord.selection
+$selection.typeText($docText)
+$selection.typeParagraph()
+$selection.typeText($docText)
+
+$wordDoc.SaveAs([REF]$filename)
+$msWord.visible = $true
+
+del $refFileName
